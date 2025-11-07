55be5c90ea7bb7f8132fcd36a2c051fd2490aae5
naveenfromap21 <yaparalanaveen9@gmail.com>
2025-11-07 09:46:34 +0530

    firstcommit

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+print("helloworld")
+for i in range(29):
+    print(i)
+name = "naveen"
+age = 18
+boolean = "True"
+
+list1 = [1,2,4,5,6,7,8]
+tuples1 = (2,4,5,6,7)
+list2 = ["naveen","jp","teju","navy"]
+dictionary1 = {"naveen":29,"jp":26,"teju":25,"navy":19}
+set1 = {1,2,3,4}
+for naveen in list1:
+    print("naveen")
+print("helloworld")
+str1 = "naveen"
+
+
+
